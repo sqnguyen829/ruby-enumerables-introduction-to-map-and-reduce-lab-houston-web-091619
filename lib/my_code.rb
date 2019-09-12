5ff748735139b1c56ed7f1beb7c2e48c3839f716
steven nguyen <sqnguyen829@gmail.com>
2019-09-12 22:05:58 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,13 +1,13 @@
 # My Code here....
 
 def map_to_negativize(source_array)
-  new_array = []
-  i = 0
-  while i < start_array.length do
-    new_array << start_array[i] * -1
-    i +=1
-  end
-  new_array
+  end_array = []
+  i=0
+    while i < start_array.length do
+      end_array << start_array[i] * 2
+      i += 1
+    end
+  end_array
 end
 
 def map_to_no_change (start_array)
